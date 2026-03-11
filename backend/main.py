5d976f3db938a3867b26b024e3da3773256a3c95
yaBobJonez <yaBobJonez@gmail.com>
2026-03-11 22:05:41 +0200

    chore: deploy web app to Nginx in Docker

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,9 @@ from routers import users, projects, tasks, documents, incidents
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173"
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r'https?://((localhost)|(127\.0\.0\.1))(:\d+)?',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
